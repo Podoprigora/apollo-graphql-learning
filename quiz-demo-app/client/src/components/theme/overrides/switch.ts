887831367739d32f0d675b9e7b888468f31eaca8
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2022-07-10 19:55:22 +0300

    feat: restyling Switch component

--- quiz-demo-app/client/src/components/theme/overrides/switch.ts
+++ quiz-demo-app/client/src/components/theme/overrides/switch.ts
@@ -1,10 +1,51 @@
-import { Theme, Components } from '@mui/material/styles';
+import { Theme, Components, alpha } from '@mui/material/styles';
 
 export const Switch = (theme: Theme): Components => {
   return {
     MuiSwitch: {
       styleOverrides: {
         root: {},
+        track: {
+          backgroundColor: '#ddd',
+          boxShadow: 'inset 0 0.1rem 0.4rem -0.1rem rgba(0,0,0, 0.22)',
+          opacity: 1,
+        },
+        thumb: {},
+        switchBase: {
+          color: theme.palette.common.white,
+        },
+        sizeMedium: {
+          padding: 0,
+          width: 50,
+          height: 28,
+
+          '& .MuiSwitch-switchBase': {
+            padding: 4,
+
+            '&.Mui-checked': {
+              transform: 'translateX(22px)',
+            },
+          },
+
+          '& .MuiSwitch-thumb': {
+            width: 20,
+            height: 20,
+          },
+
+          '& .MuiSwitch-track': {
+            borderRadius: 14,
+          },
+        },
+        colorPrimary: {
+          '&.Mui-checked': {
+            color: theme.palette.common.white,
+
+            '& + .MuiSwitch-track': {
+              backgroundColor: alpha(theme.palette.primary.main, 0.85),
+              opacity: 1,
+            },
+          },
+        },
       },
     },
   };
